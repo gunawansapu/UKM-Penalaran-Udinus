4af0f7d8f5e433f4987841b9e26d7f58968335fb
gunawansapu <gunawangsasaputra@gmail.com>
2026-08-16 20:19:45 +0700

    lppjjwjbs

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// 👇 TAMBAHKAN 2 BARIS INI SEBELUM REACT ME-RENDER WEB-NYA
+if ('scrollRestoration' in window.history) {
+  window.history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0); // Paksa ke atas seketika saat file JS baru saja di-download
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
